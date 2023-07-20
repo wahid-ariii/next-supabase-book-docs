cde22d91aa63f2c383eb71570560b1e74ffe466c
wahidari <wahiid.ari@gmail.com>
2023-07-20 18:42:41 +0700

    upgrade nextra version

--- theme.config.tsx
+++ theme.config.tsx
@@ -98,6 +98,16 @@ const config = {
     content: () => <>Question? Give us feedback </>,
     labels: 'feedback'
   },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  },
   footer: {
     text: () => {
       return (
